24114b190592b30ccd55c009ffcea1922b62b906
Colby Fayock <fayock@gmail.com>
2018-08-06 21:51:42 -0400

    updating the plugin to actually use the util imports :facepalm:, adding test for no meta high priority head

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Partial = require('./lib/partial');
+const Util = require('./lib/util');
 
 /**
  * HtmlWebpackPartialsPlugin
@@ -42,7 +43,7 @@ class HtmlWebpackPartialsPlugin {
 
           // Finally inject the partial into the HTML stream
 
-          data.html = injectPartial(data.html, {
+          data.html = Util.injectPartial(data.html, {
             options: partial.options,
             html: partial.template(partial.options),
             priority: partial.priority,
@@ -62,43 +63,3 @@ class HtmlWebpackPartialsPlugin {
 }
 
 module.exports = HtmlWebpackPartialsPlugin;
-
-
-/**
- * injectPartial
- * @description takes an html string and injects new html string based on the set priority and tag location
- */
-
-function injectPartial(base_html, { options, html, priority, location }) {
-
-  let tag;
-  let regex;
-  let replacement;
-
-  if ( location === 'head' && priority === 'high' ) {
-    tag = '<meta charset="utf-8">';
-    regex = new RegExp(tag, 'i');
-  } else {
-    tag = targetTag(priority, location);
-    regex = new RegExp(tag.replace('/', '\/'), 'i');
-  }
-
-  if ( priority === 'low' ) {
-    replacement = `${html}${tag}`;
-  } else {
-    replacement = `${tag}${html}`;
-  }
-
-  return base_html.replace(regex, replacement);
-
-}
-
-
-/**
- * targetTag
- * @description Creates an opening or closing html tag string based on priority and location
- */
-
-function targetTag(priority, location) {
-  return `<${priority === 'high' ? '' : '/'}${location}>`;
-}
