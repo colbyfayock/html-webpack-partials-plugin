158ce99532f52669109e78a00ff9df9805d113a0
Vaibhav <mailvaibhavchopra@gmail.com>
2020-09-21 14:21:23 +0530

    removed html entities and moved packages to yarn

--- examples/analytics/webpack.config.js
+++ examples/analytics/webpack.config.js
@@ -4,11 +4,11 @@ const HtmlWebpackPartialsPlugin = require('../../');
 
 module.exports = {
   entry: {
-    main: path.join(__dirname, './main.js')
+    main: path.join(__dirname, './main.js'),
   },
   output: {
     path: path.join(__dirname, './dist'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   plugins: [
     new HtmlWebpackPlugin(),
@@ -18,13 +18,13 @@ module.exports = {
         location: 'head',
         priority: 'high',
         options: {
-            // Note: Update this to your property ID
-            ga_property_id: 'UA-28076707-7'
-        }
+          // Note: Update this to your property ID
+          ga_property_id: 'UA-28076707-7',
+        },
       },
       {
-        path: path.join(__dirname, './partials/body.html')
-      }
-    ])
-  ]
+        path: path.join(__dirname, './partials/body.html'),
+      },
+    ]),
+  ],
 };
