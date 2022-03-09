0fa8fa461a160c2e602f6cedab4a48a9a8cf3821
sgb004 <sgb004@gmail.com>
2022-03-08 21:40:22 -0600

    Fix get img

--- index.js
+++ index.js
@@ -9,62 +9,104 @@ const Util = require('./lib/util');
  */
 
 class HtmlWebpackPartialsPlugin {
+	static filesProcessed = [];
 
   constructor(settings = {}) {
     this.settings = settings;
   }
 
   apply(compiler) {
+	const { webpack } = compiler;
+	const { Compilation } = webpack;
+	const { RawSource } = webpack.sources;
 
-    compiler.hooks.compilation.tap('HtmlWebpackPartialsPlugin', compilation => {
+	// If the input isn't an array, add it as one to simplify the process
 
-      HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync('HtmlWebpackPartialsPlugin', (data, callback) => {
+	if ( !Array.isArray(this.settings) ) {
+		this.settings = [ this.settings ];
+	}
 
-        // If the input isn't an array, add it as one to simplify the process
+	this.partial_collection = this.settings.map(partial => {
+		return new Partial(partial);
+	});
 
-        if ( !Array.isArray(this.settings) ) {
-          this.settings = [ this.settings ];
-        }
+	// We treat the partial as a HtmlWebpackPlugin template so that html-loader can process the partial and get the assets (example imgs)
 
-        const partial_collection = this.settings.map(partial => {
-          return new Partial(partial);
-        }).filter(partial => {
+	this.partial_collection.forEach(partial => {
+		new HtmlWebpackPlugin({
+			template: partial.path,
+			inject: false,
+			filename: partial.unique_name,
+		}).apply(compiler);
+	});
 
-          // User option to conditionally inject snippet to allow for config based
-          // injection management. Additionally check to see if the partial template
-          // filename matches the current HTML Webpack Plugin instance. This defaults
-          // to index.html if not set
+	// Get list of files processed by HtmlWebpackPlugin
 
-          return partial.should_inject && (
-            partial.template_filename === data.plugin.options.filename || 
-            Array.isArray(partial.template_filename)
-              ? partial.template_filename.includes(data.plugin.options.filename)
-              : partial.template_filename === '*'
-          );
+	compiler.hooks.compilation.tap('HtmlWebpackPartialsPlugin', compilation => {
+		HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('HtmlWebpackPartialsPlugin', (data, cb) => {
+			!HtmlWebpackPartialsPlugin.filesProcessed.includes(data.outputName) ? HtmlWebpackPartialsPlugin.filesProcessed.push(data.outputName) : '';
+			cb(null, data);
+		});
+	});
 
-        }).forEach(partial => {
+	// Use this hook and this stage to ensure that all assets were already added to the compilation by other plugins
+	// and we can use to get htmls and put partial in them
 
-          // Once we know we're using the partial, read the file and create a template
+	compiler.hooks.thisCompilation.tap('HtmlWebpackPartialsPlugin', (compilation) => {
+		compilation.hooks.processAssets.tap(
+			{
+				name: 'HtmlWebpackPartialsPlugin',
+				stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+			},
+			(assets) => {
+				this.partial_collection.forEach(partial => {
+					// Get list of files to add partial
+					let filesProcessed = partial.template_filename;
+					if(!Array.isArray(partial.template_filename)){
+						if(filesProcessed == '*'){
+							filesProcessed = HtmlWebpackPartialsPlugin.filesProcessed.filter(f => f != partial.unique_name);
+						}else{
+							filesProcessed = [partial.template_filename];
+						}
+					}
 
-          partial.createTemplate();
+					filesProcessed.forEach( template_filename => {
+						//We get the html template where the partial will be injected
+						const dataHtml = compilation.getAsset(template_filename).source._value;
+						// Inject the partial into the HTML template
+						const html = Util.injectPartial(dataHtml, {
+							options: partial.options,
+							// We get the partial and transform it in a loash template to pass options
+							html: partial.createTemplate(compilation.getAsset(partial.unique_name).source._value),
+							priority: partial.priority,
+							location: partial.location,
+						});
+	
+						compilation.updateAsset(template_filename, new RawSource(html));
+					});
+				});
+				
+			});
+		}
+	);
+	
+	// With this hook we can delete the partial before webpack convert it in a html file,
+	// at this point the partial should have already been injected into the selected template 
 
-          // Finally inject the partial into the HTML stream
-
-          data.html = Util.injectPartial(data.html, {
-            options: partial.options,
-            html: partial.template(partial.options),
-            priority: partial.priority,
-            location: partial.location,
-          });
-
-        });
-
-        callback(null, data);
-
-      });
-
-    });
-
+	compiler.hooks.thisCompilation.tap('HtmlWebpackPartialsPlugin', (compilation) => {
+		compilation.hooks.processAssets.tap(
+			{
+				name: 'HtmlWebpackPartialsPlugin',
+				stage: Compilation.PROCESS_ASSETS_STAGE_ANALYSE,
+			},
+			(assets) => {
+				this.partial_collection.forEach(partial => {
+					compilation.deleteAsset(partial.unique_name);
+				});
+			});
+		}
+	);
+	
   }
 
 }
