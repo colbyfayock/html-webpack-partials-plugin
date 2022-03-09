96b83966bc53ddc3460e862d4d845cf0e1c0cf8e
sgb004 <sgb004@gmail.com>
2022-03-09 15:01:10 -0600

    I change the way to get the index.html and other-page.html generated by webpack because  result.compilation.assets['index.html'].source() and result.compilation.assets['other-page.html'].source() no longer works

--- test/basic-multi.test.js
+++ test/basic-multi.test.js
@@ -12,12 +12,12 @@ describe('Basic Multi', function() {
 
       expect(error).to.equal(null);
 
-      const index_html = result.compilation.assets['index.html'].source();
+      const index_html = fs.readFileSync(path.resolve(__dirname, '../examples/basic-multi/dist/index.html')).toString();
       const index_fixture = fs.readFileSync(path.resolve(__dirname, 'fixtures/basic-multi-index.html')).toString();
 
       expect(index_html).to.equal(index_fixture);
-
-      const other_html = result.compilation.assets['other-page.html'].source();
+		
+      const other_html = fs.readFileSync(path.resolve(__dirname, '../examples/basic-multi/dist/other-page.html')).toString();
       const other_fixture = fs.readFileSync(path.resolve(__dirname, 'fixtures/basic-multi-other.html')).toString();
 
       expect(other_html).to.equal(other_fixture);
